70fcc36287293c123c85b58a4c0306d40778fbee
Andrew Krueger <akrueger@adjacent-tech.com>
2019-04-30 22:35:32 -0500

    Temporary changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,11 +9,60 @@ import { Observable, of } from 'rxjs'
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  groups$: Observable<any>
   games$: Observable<any>
+  game$: Observable<any>
+  users$: Observable<any>
 
   constructor(private restService: RestService) {}
 
   getGames() {
     this.restService.getGames().subscribe(games => (this.games$ = of(games)))
+  }
+
+  getGroupsByUserId(userId: number) {
+    this.restService
+      .getGroupsByUserId(userId)
+      .subscribe(groups => (this.groups$ = of(groups)))
+  }
+
+  createGroup(groupName: string, userId: number) {
+    this.restService
+      .createGroup(groupName, userId)
+      .subscribe(groups => (this.groups$ = of(groups)))
+  }
+
+  getGameById(gameId: number) {
+    this.restService
+      .getGameById(gameId)
+      .subscribe(game => (this.game$ = of(game)))
+  }
+
+  getGameByName(gameName: string) {
+    this.restService
+      .getGameByName(gameName)
+      .subscribe(game => (this.game$ = of(game)))
+  }
+
+  createGame(gameName: string) {
+    this.restService
+      .createGame(gameName)
+      .subscribe(game => (this.game$ = of(game)))
+  }
+
+  deleteGameById(gameId: number) {
+    this.restService
+      .deleteGameById(gameId)
+      .subscribe(game => (this.game$ = of(game)))
+  }
+
+  deleteGameByName(gameName: string) {
+    this.restService
+      .deleteGameByName(gameName)
+      .subscribe(game => (this.game$ = of(game)))
+  }
+
+  getUsers() {
+    this.restService.getUsers().subscribe(users => (this.users$ = of(users)))
   }
 }
