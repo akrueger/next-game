d0bb957023fef219a9bc8d4d1e53438c394cf2c9
Andrew Krueger <akrueger@adjacent-tech.com>
2019-05-14 00:43:57 -0500

    Inject reducers

--- src/app/modules/games/games.module.ts
+++ src/app/modules/games/games.module.ts
@@ -1,22 +1,34 @@
 // Angular
-import { NgModule } from '@angular/core'
+import { NgModule, InjectionToken } from '@angular/core'
 // Application Modules
 import { SharedModule } from '../shared/shared.module'
 // ngrx
-import { StoreModule } from '@ngrx/store'
+import { StoreModule, ActionReducerMap } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
 // Reducer
 import { reducer } from './games.reducer'
+// Models
+import { State } from './game.model'
 // Services
 import { GamesService } from './games.service'
 import { GamesEffectsService } from './games-effects.service'
 
+export const FEATURE_REDUCER_TOKEN = new InjectionToken<
+  ActionReducerMap<State>
+>('Feature Reducers')
+
 @NgModule({
   imports: [
-    StoreModule.forFeature('games', reducer),
+    StoreModule.forFeature('games', FEATURE_REDUCER_TOKEN),
     EffectsModule.forFeature([GamesEffectsService]),
     SharedModule
   ],
-  providers: [GamesService]
+  providers: [
+    {
+      provide: FEATURE_REDUCER_TOKEN,
+      useValue: reducer
+    },
+    GamesService
+  ]
 })
 export class GamesModule {}
--- src/app/modules/groups/groups.module.ts
+++ src/app/modules/groups/groups.module.ts
@@ -1,28 +1,43 @@
 // Angular
-import { NgModule } from '@angular/core'
-import { GroupComponent } from './group.component'
+import { NgModule, InjectionToken } from '@angular/core'
 // Application Modules
 import { SharedModule } from '../shared/shared.module'
 import { GroupsRoutingModule } from './groups.routing'
 // ngrx
-import { StoreModule } from '@ngrx/store'
+import { StoreModule, ActionReducerMap } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
 // Reducer
 import { reducer } from './groups.reducer'
+// Components
+import { GroupComponent } from './group.component'
+// Models
+import { State } from './group.model'
 // Services
 import { GroupsService } from './groups.service'
+import { GroupsEffectsService } from './groups-effects.service'
 // Guards
 import { GroupGuard } from './group.guard'
 
+export const FEATURE_REDUCER_TOKEN = new InjectionToken<
+  ActionReducerMap<State>
+>('Feature Reducers')
+
 @NgModule({
   imports: [
-    StoreModule.forFeature('groups', reducer),
-    EffectsModule.forFeature([]),
+    StoreModule.forFeature('groups', FEATURE_REDUCER_TOKEN),
+    EffectsModule.forFeature([GroupsEffectsService]),
     SharedModule,
     GroupsRoutingModule
   ],
   exports: [GroupComponent],
   declarations: [GroupComponent],
-  providers: [GroupsService, GroupGuard]
+  providers: [
+    {
+      provide: FEATURE_REDUCER_TOKEN,
+      useValue: reducer
+    },
+    GroupsService,
+    GroupGuard
+  ]
 })
 export class GroupsModule {}
